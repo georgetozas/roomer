c3bb3be852148a91d4806746298cfe0e5204155b
George Tozakidis <gredo2tero@gmail.com>
2025-08-20 10:26:23 +0300

    chore(types): local declaration for 'qrcode'

--- types/qrcode.d.ts
+++ types/qrcode.d.ts
@@ -1,1 +1,9 @@
-declare module 'qrcode';
+declare module 'qrcode' {
+  // Minimal surface we use
+  const QRCode: {
+    toBuffer: (text: string, opts?: any) => Promise<Buffer>;
+    toDataURL: (text: string, opts?: any) => Promise<string>;
+    toString: (text: string, opts?: any) => Promise<string>;
+  };
+  export default QRCode;
+}
